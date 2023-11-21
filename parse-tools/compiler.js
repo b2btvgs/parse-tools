df454d5dcf7d06351e96d2cdcdba27e490674c85
Paul Mccranie <paulmccranie@Pauls-MBP.lan>
2023-11-20 19:01:09 -0800

    initial frameout of parse-tools logic

---
+++ parse-tools/compiler.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const START_TOKEN = "***start***";
+const END_TOKEN = "***end***";
+
+const compileFiles = (dirPath, outputFile) => {
+  let compiledContent = "";
+
+  const traverseDirectory = (dir) => {
+    fs.readdirSync(dir).forEach((file) => {
+      const filePath = path.join(dir, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        traverseDirectory(filePath); // Recursive call for directories
+      } else {
+        const fileContent = fs.readFileSync(filePath, "utf8");
+        const relativePath = path.relative(__dirname, filePath);
+        compiledContent += `${START_TOKEN}\n${relativePath}\n${fileContent}\n${END_TOKEN}\n\n`;
+      }
+    });
+  };
+
+  traverseDirectory(dirPath);
+
+  try {
+    fs.writeFileSync(outputFile, compiledContent);
+  } catch (error) {
+    console.error("Error writing the compiled file:", error.message);
+  }
+};
+
+// Replace 'src/data-transfer' with the actual path of the directory to be compiled
+// and 'path/to/your/compiledFile.txt' with the desired output file path
+compileFiles("src/data-transfer", "path/to/your/compiledFile.txt");
